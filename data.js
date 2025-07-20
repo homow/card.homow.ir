03e3251228db26153b1e44be9cda5d33627ed6e9
homow <homowsh897@gmail.com>
2025-07-21 00:54:17 +0330

    homow-style: new structure + visual + meta updates

---
+++ data.js
@@ -0,0 +1,154 @@
+const data = [
+    {
+        name: "website",
+        link: "https://homow.ir",
+        icon: "fa-solid fa-globe slideIn-Animate",
+        color: "var(--light-blue)",
+        detail: "homow.ir"
+    },
+    {
+        name: "github",
+        link: "https://github.com/homow",
+        icon: "fa-brands fa-github",
+        color: "var(--light-blue)",
+        detail: "github.com/homow"
+    },
+    {
+        name: "phone1",
+        link: "tel:+989036364078",
+        icon: "fa-solid fa-phone",
+        color: "var(--green-color)",
+        detail: "+98 903 636 4078"
+    },
+    {
+        name: "phone2",
+        link: "tel:+989210629512",
+        icon: "fa-solid fa-phone",
+        color: "var(--green-color)",
+        detail: "+98 921 062 9512"
+    },
+    {
+        name: "telegram",
+        link: "https://t.me/homow_dev",
+        icon: "fa-brands fa-telegram",
+        color: "var(--blue-telegram)",
+        detail: "@homow_dev"
+    },
+    {
+        name: "channel",
+        link: "https://t.me/HomowDev",
+        icon: "fa-brands fa-telegram",
+        color: "var(--blue-telegram)",
+        detail: "@HomowDev"
+    },
+    {
+        name: "telPort",
+        link: "https://t.me/homow_portfolio",
+        icon: "fa-brands fa-telegram",
+        color: "var(--blue-telegram)",
+        detail: "@homow_portfolio"
+    },
+    {
+        name: "telGroup",
+        link: "https://t.me/homow_chat",
+        icon: "fa-brands fa-telegram",
+        color: "var(--blue-telegram)",
+        detail: "@homow_chat"
+    },
+    {
+        name: "instagram",
+        link: "https://www.instagram.com/homow_dev",
+        icon: "fa-brands fa-instagram",
+        color: "#9929EA",
+        detail: "@homow_dev"
+    },
+    {
+        name: "whatsapp",
+        link: "https://wa.me/989210629512",
+        icon: "fa-brands fa-whatsapp",
+        color: "var(--green-color)",
+        detail: "+98 921 062 9512"
+    },
+    {
+        name: "proton",
+        link: "mailto:homow_dev@proton.me",
+        icon: "fa-solid fa-envelope",
+        color: "var(--light-blue)",
+        detail: "homow_dev@proton.me"
+    },
+    {
+        name: "gmail",
+        link: "mailto:homayoun.sharifi80@gmail.com",
+        icon: "fa-solid fa-envelope",
+        color: "var(--light-blue)",
+        detail: "homayoun.sharifi80@gmail.com"
+    },
+    {
+        name: "linkedin",
+        link: "https://www.linkedin.com/in/homow/",
+        icon: "fa-brands fa-linkedin",
+        color: "var(--blue-telegram)",
+        detail: "linkedin.com/in/homow/"
+    },
+    {
+        name: "facebook",
+        link: "https://www.facebook.com/homow.dev",
+        icon: "fa-brands fa-facebook",
+        color: "var(--blue-telegram)",
+        detail: "facebook.com/homow.dev"
+    }
+];
+
+const EN = {
+    website: "Website",
+    github: "GitHub",
+    phone1: "Phone Number 1",
+    phone2: "Phone Number 2",
+    telegram: "Telegram",
+    channel: "Channel",
+    telPort: "Portfolio",
+    telGroup: "Telegram Chats",
+    instagram: "Instagram",
+    whatsapp: "Whatsapp",
+    proton: "Email",
+    gmail: "Gmail",
+    linkedin: "LinkedIn",
+    facebook: "Facebook",
+}
+
+const FA = {
+    website: "وبسایت",
+    github: "گیت هاب",
+    phone1: "شماره تماس اول",
+    phone2: "شماره تماس دوم",
+    telegram: "صفحه تلگرام",
+    channel: "چنل تلگرام",
+    telPort: "چنل نمونه کارها",
+    telGroup: "گروه تلگرامی",
+    instagram: "اینستاگرام",
+    whatsapp: "واتساپ",
+    proton: "ایمیل",
+    gmail: "جیمیل",
+    linkedin: "لینکدین",
+    facebook: "فیسبوک",
+}
+
+const bodyEN = {
+    bodyTitle: "Homayoun Sharifi",
+    bodySub: "Web Developer & WordPress Developer | Programming Projects | Academic Projects",
+    linkWeb: "My Website",
+    linkAbout: "About Me",
+    linkPort: "My Portfolio",
+    linkFooter: "<span>Designed With</span> <i style='color:#C0392B;' class='fa-solid fa-heart-pulse'></i> By Homow"
+}
+
+const bodyFA = {
+    bodyTitle: "همایون شریفی",
+    bodySub: "توسعه دهنده وبسایت معمولی و وردپرسی | پروژه های برنامه نویسی | پروژه های دانشجویی",
+    linkWeb: "وبسایت",
+    linkAbout: "درباره من",
+    linkPort: "نمونه کارها",
+    linkFooter: "<span>ساخته شده با</span> <i style='color:#C0392B;' class='fa-solid fa-heart-pulse'></i> توسط همایون شریفی"
+}
+
+export {data, EN, FA, bodyEN, bodyFA}
--- index.js
+++ index.js
@@ -1,1 +1,87 @@
-console.log('Happy developing ✨')
+import * as info from "/data.js";
+
+const textBodyElem = document.querySelectorAll(".bodyTranslate")
+const menuLinks = document.querySelectorAll(".menu-items a")
+const linksIcon = document.querySelectorAll(".menu-items a .fa-arrow-up-right-from-square")
+const contactWrapper = document.querySelector(".contact")
+const langBtn = document.getElementById("lang")
+const text = langBtn.querySelector("span")
+const currentLang = localStorage.getItem("lang") || "FA"
+
+const changeBodyDir = () => {
+    const lang = localStorage.getItem("lang") || "FA"
+    menuLinks.forEach(link => {
+        link.classList.toggle("dir-eng", lang === "EN")
+    })
+
+    linksIcon.forEach(elem => {
+        elem.classList.toggle("dir-eng-icon", lang === "EN")
+    })
+
+    document.querySelector(".contact-wrapper").classList.toggle("dir-eng", lang === "EN")
+}
+
+const setText = () => {
+    const lang = localStorage.getItem("lang") || "FA"
+    const obj = lang === "FA" ? info["bodyFA"] : info["bodyEN"]
+
+    textBodyElem.forEach(elem => {
+        if (obj[elem.id]) {
+            elem.innerHTML = obj[elem.id]
+        }
+    })
+    changeBodyDir()
+}
+
+const translate = () => {
+    const currentLang = localStorage.getItem("lang") || "FA"
+    const newLang = currentLang === "FA" ? "EN" : "FA"
+    localStorage.setItem("lang", newLang)
+    text.textContent = currentLang;
+    createItems(info[newLang])
+    changDir()
+    setText()
+}
+
+const changDir = () => {
+    const newLang = localStorage.getItem("lang") || "FA"
+    const links = document.querySelectorAll(".contact-box-wrapper a span:first-child")
+    links.forEach(link => {
+        link.dir = newLang === "FA" ? "rtl" : "ltr"
+    })
+}
+
+const createTemplate = (data, lang) => {
+    const wrapper = document.createElement('div');
+    wrapper.className = "contact-wrapper";
+
+    data.forEach(d => {
+        const template = `<div class="contact-box-wrapper">
+    <a href="${d.link}" target="_blank">
+        <span dir="rtl">
+            <i style="color: ${d.color}" class="${d.icon}"></i>
+            ${lang[d.name]}
+        </span>
+        <span>${d.detail}</span>
+    </a>
+</div>`
+        wrapper.insertAdjacentHTML("beforeend", template);
+    })
+    return wrapper;
+}
+
+const createItems = lang => {
+    contactWrapper.innerHTML = "";
+    const elements = createTemplate(info.data, lang);
+    contactWrapper.appendChild(elements)
+}
+
+const setLang = lang => {
+    createItems(lang === "FA" ? info.FA : info.EN)
+    text.textContent = lang === "FA" ? "EN" : "FA"
+    changDir()
+    setText()
+}
+
+setLang(currentLang) // set language after load page
+langBtn.addEventListener("click", translate) // button translate
